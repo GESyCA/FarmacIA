c43aa08b15efc2a034d8d309e4c12625bf4659a9
Pablo <lucasrn03@gmail.com>
2024-11-14 11:29:41 -0300

    Mesclando repositório local em FarmacIA/Paciente

---
+++ Paciente/utils/bulario.py
@@ -0,0 +1,46 @@
+import subprocess
+import json, os
+
+# Verifica se o arquivo existe
+def verificar_arquivo(caminho_arquivo):
+    if os.path.isfile(caminho_arquivo):
+        return True
+    else:
+        return False
+    
+def buscar_remedio(nome, pagina=1):
+    try:
+        # Executa o script JavaScript passando o nome do remédio e a página como argumentos
+        result = subprocess.run(
+            ['node', 'utils/bulario.js', nome, str(pagina)],
+            capture_output=True,
+            text=True,
+            encoding='utf-8'
+        )
+        
+        # Verifica se houve erros na execução
+        if result.returncode != 0:
+            raise Exception(f"Erro: {result.stderr}")
+        
+        # Converte a saída JSON para um dicionário Python
+        resultado = json.loads(result.stdout)
+        return resultado
+    
+    except Exception as e:
+        print(f"Erro ao buscar o remédio: {e}")
+        return None
+
+def salvar_pdf(buffer_data, filename):
+    try:
+        print("Salvando o PDF da bula...")
+        # Converte a lista de inteiros de volta para bytes
+        pdf_bytes = bytes(buffer_data)
+        
+        # Escreve o conteúdo binário no arquivo
+        with open(f'bulas_pdf/{filename}', 'wb') as f:
+            f.write(pdf_bytes)
+        
+        print(f"PDF salvo como {filename}")
+    
+    except Exception as e:
+        print(f"Erro ao salvar o PDF: {e}")
