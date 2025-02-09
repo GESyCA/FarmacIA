55397d725c29d3d6ced22fe18b29e5cb4a79a465
joas-barros <joas.barros14@hotmail.com>
2025-02-09 15:40:46 -0300

    fix: handle timeout exception

--- Paciente/farmacia/lib/app/data/repositories/chat_repository.dart
+++ Paciente/farmacia/lib/app/data/repositories/chat_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:farmacia/app/data/http/http_client.dart';
 import 'package:farmacia/app/data/models/answer_model.dart';
 import 'package:farmacia/app/data/models/question_model.dart';
@@ -8,13 +10,21 @@ class ChatRepository {
   ChatRepository({required this.httpClient});
 
   Future<Answer> sendMessage(QuestionModel question) async {
-    final response = await httpClient.post(
-      '/perguntar',
-      body: question.toJson(),
-    );
-    if (response.statusCode == 200) {
-      return Answer.fromJson(response.body as Map<String, dynamic>);
-    } else {
+    try {
+      final response = await httpClient
+          .post(
+            '/perguntar',
+            body: question.toJson(),
+          )
+          .timeout(const Duration(seconds: 5));
+      if (response.statusCode == 200) {
+        return Answer.fromJson(response.body as Map<String, dynamic>);
+      } else {
+        return Answer(resposta: 'Desculpe, não entendi a pergunta');
+      }
+    } on TimeoutException {
+      return Answer(resposta: 'Desculpe, tempo foi exedido');
+    } catch (e) {
       return Answer(resposta: 'Desculpe, não entendi a pergunta');
     }
   }
