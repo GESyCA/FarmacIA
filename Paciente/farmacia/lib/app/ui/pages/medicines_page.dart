76417adf12f713943321a1e352f535921a00c277
joas-barros <joas.barros14@hotmail.com>
2025-03-06 10:34:40 -0300

    feat: start medicine page

--- Paciente/farmacia/lib/app/ui/pages/medicines_page.dart
+++ Paciente/farmacia/lib/app/ui/pages/medicines_page.dart
@@ -1,3 +1,4 @@
+import 'package:farmacia/app/data/models/medicine_model.dart';
 import 'package:flutter/material.dart';
 
 class MedicinesPage extends StatefulWidget {
@@ -8,6 +9,14 @@ class MedicinesPage extends StatefulWidget {
 }
 
 class _MedicinesPageState extends State<MedicinesPage> {
+  final List<Medicine> _medicines = [
+    Medicine(id: 1, name: "Paracetamol", nextDate: "10/10/2025 07:00"),
+    Medicine(id: 2, name: "Ibuprofeno", nextDate: "10/10/2025 07:00"),
+    Medicine(id: 3, name: "Omeprazol", nextDate: "10/10/2025 07:00"),
+    Medicine(id: 4, name: "Amoxilinina", nextDate: "10/10/2025 07:00"),
+    Medicine(id: 5, name: "Dipirona", nextDate: "10/10/2025 07:00"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,22 +34,77 @@ class _MedicinesPageState extends State<MedicinesPage> {
           ),
         ],
       ),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'Welcome to the Medicines Page',
-              ),
-            ],
-          ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
         ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 16,
+            ),
+            _searchBar(),
+            SizedBox(
+              height: 16,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _medicines.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.edit,
+                        color: Colors.red,
+                      ),
+                      title: Text(_medicines[index].name),
+                      subtitle: Text(_medicines[index].nextDate),
+                      trailing: IconButton(
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _medicines.removeAt(index);
+                          });
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  TextField _searchBar() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Buscar por medicamento",
+        prefixIcon: Icon(Icons.search),
+        suffixIcon: Icon(Icons.mic),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Color(0xFFCBD5E1)),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Color(0xFFCBD5E1)),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide(color: Color(0xFFCBD5E1), width: 2),
+        ),
+        filled: true,
+        fillColor: Colors.white,
       ),
     );
   }
