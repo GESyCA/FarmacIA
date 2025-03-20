1001676e5167409827e1c5d3de26f2cb813e2911
joas-barros <joas.barros14@hotmail.com>
2025-03-20 09:38:22 -0300

    feat: first layout

--- Paciente/farmacia/lib/app/ui/pages/add_medicine_page.dart
+++ Paciente/farmacia/lib/app/ui/pages/add_medicine_page.dart
@@ -10,6 +10,40 @@ class AddMedicinePage extends StatefulWidget {
 class _AddMedicinePageState extends State<AddMedicinePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text("Cadastro"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              Navigator.pushNamed(context, '/login');
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        padding: const EdgeInsets.all(14),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+        ),
+        child: Column(
+          children: [
+            Text(
+              "Nome do medicamento",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
