4d9e1099c647dfc84c18da4deed0bf2ebf09c5e5
joas-barros <joas.barros14@hotmail.com>
2025-03-21 13:02:09 -0300

    feat: add Buttons

--- Paciente/farmacia/lib/app/ui/pages/add_medicine_page.dart
+++ Paciente/farmacia/lib/app/ui/pages/add_medicine_page.dart
@@ -69,7 +69,9 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
                       height: 8,
                     ),
                     _dropDown(
-                        "Dipirona", ["Dipirona", "Paracetamol", "Ibuprofeno"]),
+                        "Dipirona",
+                        ["Dipirona", "Paracetamol", "Ibuprofeno"],
+                        selectedMedication!),
                     SizedBox(
                       height: 32,
                     ),
@@ -91,8 +93,10 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
                     SizedBox(
                       height: 8,
                     ),
-                    _dropDown("3 vezes ao dia",
-                        ["3 vezes ao dia", "2 vezes ao dia", "1 vez ao dia"]),
+                    _dropDown(
+                        "3 vezes ao dia",
+                        ["3 vezes ao dia", "2 vezes ao dia", "1 vez ao dia"],
+                        selectedFrequency!),
                     SizedBox(
                       height: 32,
                     ),
@@ -139,7 +143,8 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
                     SizedBox(
                       height: 8,
                     ),
-                    _dropDown("Comprimido", ["Comprimido", "Gotas", "Pomada"]),
+                    _dropDown("Comprimido", ["Comprimido", "Gotas", "Pomada"],
+                        selectedForm!),
                     SizedBox(
                       height: 32,
                     ),
@@ -150,16 +155,19 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        _inputDate(context, "Date", _dateController),
+                        _inputDate(context, "Inicio", _dateController),
                         SizedBox(width: 10),
-                        _inputDate(context, "End Date", _endDateController),
+                        _inputDate(context, "Fim", _endDateController),
                       ],
                     ),
-                    SizedBox(
-                      height: 8,
-                    ),
                     CheckboxListTile(
-                      title: Text("Tempo indeterminado"),
+                      title: Text(
+                        "Tempo indeterminado",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[600],
+                        ),
+                      ),
                       value: _indeterminate,
                       activeColor: Colors.red,
                       onChanged: (bool? value) {
@@ -172,11 +180,74 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
                   ],
                 ),
               ),
+              SizedBox(
+                height: 16,
+              ),
+              // Buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _cancelButton(context),
+                  _saveButton(context),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  ElevatedButton _saveButton(BuildContext context) {
+    return ElevatedButton(
+                  onPressed: () {
+                    // Save medicine
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFB9160C),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 12,
+                    ),
+                  ),
+                  child: Text(
+                    "Concluir",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+  }
+
+  ElevatedButton _cancelButton(BuildContext context) {
+    return ElevatedButton(
+                  onPressed: () => Navigator.pop(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      side: BorderSide(color: Color(0xFFB9160C)),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 12,
+                    ),
+                  ),
+                  child: Text(
+                    "Cancelar",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xFFB9160C),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
   }
 
   Expanded _inputDate(
@@ -197,10 +268,15 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
             borderSide: BorderSide(color: Colors.red),
           ),
         ),
         readOnly: true,
+        enabled: !_indeterminate,
         onTap: () => _selectDate(context, controller),
       ),
     );
@@ -215,7 +291,7 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
       lastDate: DateTime(2100),
     );
     if (pickedDate != null) {
-      String formattedDate = DateFormat('MM/dd/yyyy').format(pickedDate);
+      String formattedDate = DateFormat('dd/MM/yyyy').format(pickedDate);
       setState(() {
         controller.text = formattedDate;
       });
@@ -261,12 +337,15 @@ class _AddMedicinePageState extends State<AddMedicinePage> {
     );
   }
 
-  DropdownButtonFormField<String> _dropDown(
-      String? selectedMedication, List<String> selectItems) {
+  DropdownButtonFormField<String> _dropDown(String? selectedMedication,
+      List<String> selectItems, String controlVariable) {
     return DropdownButtonFormField<String>(
       value: selectedMedication, // Default selected value
       onChanged: (newValue) {
         // Handle value change
+        setState(() {
+          controlVariable = newValue!;
+        });
       },
       items: selectItems
           .map((med) => DropdownMenuItem(
