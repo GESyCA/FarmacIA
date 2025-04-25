fcf0c21b4244b0b844509e3dcf1621d7505a7461
joas-barros <joas.barros14@hotmail.com>
2025-04-24 22:28:36 -0300

    refactor: quiz card icons

--- Paciente/farmacia/lib/app/ui/pages/chat_page.dart
+++ Paciente/farmacia/lib/app/ui/pages/chat_page.dart
@@ -17,7 +17,7 @@ class _ChatPageState extends State<ChatPage> {
 
   final ChatRepository _chatRepository = ChatRepository(
     httpClient: HttpClient(
-      baseUrl: "https://1d86-187-19-156-72.ngrok-free.app",
+      baseUrl: "https://6c07-187-19-156-72.ngrok-free.app",
     ),
   );
 
