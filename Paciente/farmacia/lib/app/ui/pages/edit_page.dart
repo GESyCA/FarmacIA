5a9e784dcd1fbf2b986a25a8501d9498c53a93ab
joas-barros <joas.barros14@hotmail.com>
2025-08-06 20:08:19 -0300

    init branch

---
+++ Paciente/farmacia/lib/app/ui/pages/edit_page.dart
@@ -0,0 +1,12 @@
+import 'package:farmacia/app/controllers/edit_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class EditPage extends GetView<EditController> {
+  const EditPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
