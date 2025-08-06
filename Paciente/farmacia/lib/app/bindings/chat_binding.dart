903c52e2acd88e7502e9307db96f383d9073b674
joas-barros <joas.barros14@hotmail.com>
2025-08-06 16:28:01 -0300

    refactor: chat page logic

---
+++ Paciente/farmacia/lib/app/bindings/chat_binding.dart
@@ -0,0 +1,19 @@
+import 'package:farmacia/app/controllers/chat_controller.dart';
+import 'package:farmacia/app/data/http/http_client.dart';
+import 'package:farmacia/app/data/repositories/chat_repository.dart';
+import 'package:get/get.dart';
+
+class ChatBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ChatController>(
+      () => ChatController(
+        ChatRepository(
+          httpClient: HttpClient(
+            baseUrl: "https://765ba1469829.ngrok-free.app",
+          ),
+        ),
+      ),
+    );
+  }
+}
