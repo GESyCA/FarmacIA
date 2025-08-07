c31cf156b9a6dd9c346b294ac287302007c29708
joas-barros <joas.barros14@hotmail.com>
2025-08-07 01:45:12 -0300

    style: no medicine registered

---
+++ Paciente/farmacia/lib/app/ui/widgets/no_medicine_registered.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NoMedicineRegistered extends StatelessWidget {
+  const NoMedicineRegistered({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Sem medicamentos cadastrados',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Comece agora o acompanhamento do seu tratamento',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
+              ),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Image.asset(
+              'assets/waiting.png',
+              height: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
