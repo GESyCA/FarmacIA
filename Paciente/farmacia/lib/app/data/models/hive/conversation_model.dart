070de24750f33e1f1a60c077d58aacde442fd38c
joas-barros <joas.barros14@hotmail.com>
2025-08-07 10:35:54 -0300

    feat: hive models

---
+++ Paciente/farmacia/lib/app/data/models/hive/conversation_model.dart
@@ -0,0 +1,46 @@
+import 'package:hive/hive.dart';
+
+part 'conversation_model.g.dart'; // Este arquivo será gerado
+
+@HiveType(typeId: 1)
+class ChatMessage extends HiveObject {
+  @HiveField(0)
+  final String text;
+
+  @HiveField(1)
+  final bool isUserMessage;
+
+  @HiveField(2)
+  final DateTime timestamp;
+
+  ChatMessage({
+    required this.text,
+    required this.isUserMessage,
+    required this.timestamp,
+  });
+}
+
+@HiveType(typeId: 2)
+class Conversation extends HiveObject {
+  @HiveField(0)
+  String? conversationId; // Pode ser nulo no início
+
+  @HiveField(1)
+  final String medicineName;
+
+  @HiveField(2)
+  final String userId;
+
+  @HiveField(3)
+  HiveList<ChatMessage> messages;
+
+  Conversation({
+    this.conversationId,
+    required this.medicineName,
+    required this.userId,
+    required this.messages,
+  });
+
+  // Chave única para salvar/recuperar no Hive
+  String get boxKey => '${userId}_$medicineName';
+}
---
+++ Paciente/farmacia/lib/app/data/models/hive/conversation_model.g.dart
@@ -0,0 +1,90 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'conversation_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ChatMessageAdapter extends TypeAdapter<ChatMessage> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ChatMessage read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ChatMessage(
+      text: fields[0] as String,
+      isUserMessage: fields[1] as bool,
+      timestamp: fields[2] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ChatMessage obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.text)
+      ..writeByte(1)
+      ..write(obj.isUserMessage)
+      ..writeByte(2)
+      ..write(obj.timestamp);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ChatMessageAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class ConversationAdapter extends TypeAdapter<Conversation> {
+  @override
+  final int typeId = 2;
+
+  @override
+  Conversation read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Conversation(
+      conversationId: fields[0] as String?,
+      medicineName: fields[1] as String,
+      userId: fields[2] as String,
+      messages: (fields[3] as HiveList).castHiveList(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Conversation obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.conversationId)
+      ..writeByte(1)
+      ..write(obj.medicineName)
+      ..writeByte(2)
+      ..write(obj.userId)
+      ..writeByte(3)
+      ..write(obj.messages);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ConversationAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
