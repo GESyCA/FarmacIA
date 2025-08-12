1f98e07cfb330c94d3189085bfca752b9507a106
joas-barros <joas.barros14@hotmail.com>
2025-08-12 18:06:52 -0300

    fix: base url

--- Paciente/farmacia/lib/app/data/repositories/chat_repository.dart
+++ Paciente/farmacia/lib/app/data/repositories/chat_repository.dart
@@ -13,7 +13,7 @@ class ChatRepository {
     try {
       final response = await httpClient
           .post(
-            '/perguntar',
+            'perguntar',
             body: question.toJson(),
           );
           //.timeout(const Duration(seconds: 5));
@@ -22,6 +22,7 @@ class ChatRepository {
       } else if (response.statusCode == 415) {
         return Answer(resposta: 'Desculpe, não entendi a pergunta', conversationId: "");
       } else {
+        print("Erro: ${response.statusCode}");
         return Answer(resposta: 'Erro de conexão', conversationId: "");
       }
     } on TimeoutException {
